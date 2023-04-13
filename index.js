50fb04d6f75814c497d41637ae074fd35d5e700c
Marisilia Garcia <marisiliaemilia@gmail.com>
2023-04-12 23:56:43 -0500

    saves changes

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let random = 1
+Math.floor(Math.random() * 6) + 1;
+
+let num3 = 24
+let num4 = 6
+let mod = num3 / num4 
+
+let max = 20
+Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
